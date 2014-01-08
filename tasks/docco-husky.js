b3da60758808e658b3e4141910e71a90a267baf0
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-01-08 11:25:09 +0100

    migrate docco-husky task to Grunt 0.4.x API

--- tasks/docco-husky.js
+++ tasks/docco-husky.js
@@ -11,30 +11,18 @@ module.exports = function(grunt) {
 
   // ### TASKS
   grunt.registerMultiTask('docco_husky', 'Docco-husky processor.', function() {
-    var options, tmp, cmd_options = [], args;
+    var cmd_options = [];
 
-    // Getting the configs.
-    tmp = grunt.config(['docco-husky', this.target, 'options']);
-    if (typeof tmp === 'object') {
-      grunt.verbose.writeln('Using "' + this.target + '" Docco-husky options.');
-      options = tmp;
-    } else {
-      grunt.verbose.writeln('Using master Docco-huskey options.');
-      args = grunt.config('docco_husky');
-      cmd_options.push('-name');
-      cmd_options.push(args.project_name);
-      _.each(args.files, function(path){
-        cmd_options.push(path);
-      });
+    cmd_options.push('-name');
+    cmd_options.push(this.data.project_name);
 
-      options = cmd_options;
-    }
-
-    grunt.verbose.writeflags(options, 'Options');
+    var paths = grunt.file.expand(this.data.files.src);
+    _.each(paths, function(path){
+      cmd_options.push(path);
+    });
 
     var done = this.async();
-    var src = grunt.file.expandFiles(this.file.src);
-    docco.document(options, function(err, result, code){
+    docco.document(cmd_options, function(err, result, code){
       grunt.log.writeln("Doccoed husky [" + src.join(", ") + "]; " + err ? err : "(No errors)" + "\n" + result + " " + code);
       done();
     });
